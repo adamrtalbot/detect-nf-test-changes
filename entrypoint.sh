9b0781580f4e60a857988efbd713dbf584f30be5
adamrtalbot <12817534+adamrtalbot@users.noreply.github.com>
2025-03-26 14:18:09 +0000

    Switch to spaced args for better handling on the CLI

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ set -eux
 git config --global --add safe.directory '*'
 
 python /entrypoint.py \
-    ${ROOT:+"-p=$ROOT"} \
-    ${HEAD:+"-r=$HEAD"} \
-    ${BASE:+"-b=$BASE"} \
-    ${IGNORED:+"-x=$IGNORED"} \
-    ${INCLUDE:+"-i=$INCLUDE"} \
-    ${LOG_LEVEL:+"--log-level=$LOG_LEVEL"} \
-    ${TYPES:+"-t=$TYPES"} \
-    ${N_PARENTS:+"--n_parents=$N_PARENTS"} \
-    ${TAGS:+"-T=$TAGS"} \
-    ${EXCLUDE_TAGS:+"--exclude_tags=$EXCLUDE_TAGS"}
+    ${ROOT:+"-p $ROOT"} \
+    ${HEAD:+"-r $HEAD"} \
+    ${BASE:+"-b $BASE"} \
+    ${IGNORED:+"-x $IGNORED"} \
+    ${INCLUDE:+"-i $INCLUDE"} \
+    ${LOG_LEVEL:+"--log-level $LOG_LEVEL"} \
+    ${TYPES:+"-t $TYPES"} \
+    ${N_PARENTS:+"--n_parents $N_PARENTS"} \
+    ${TAGS:+"-T $TAGS"} \
+    ${EXCLUDE_TAGS:+"--exclude_tags $EXCLUDE_TAGS"}
